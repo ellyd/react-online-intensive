e712c391bbc386ffc7880ade63679e7d33e44ca2
ellyd <meloniakfs27@gmail.com>
2019-09-04 20:09:37 +0300

    classwork 10

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,6 @@
 //Core
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition, TransitionGroup } from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 //Components
@@ -199,13 +199,24 @@ export default class Feed extends Component {
 
         const postsJSX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post 
-                        { ...post } 
-                        _likePost = { this._likePost } 
-                        _removePost = { this._removePost } 
-                    />
-                </Catcher>
+                <CSSTransition 
+                    classNames = { {
+                        enter: Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                    } }
+                    key = { post.id } 
+                    timeout = { {
+                        enter: 500,
+                        exit: 400,
+                } }>
+                    <Catcher>
+                        <Post 
+                            { ...post } 
+                            _likePost = { this._likePost } 
+                            _removePost = { this._removePost } 
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -229,7 +240,7 @@ export default class Feed extends Component {
                 onEntered = { this._animatePostmanEntered }>
                 <Postman />
             </Transition>
-            {postsJSX}
+            <TransitionGroup>{postsJSX}</TransitionGroup>
           </section>
         );
     }
