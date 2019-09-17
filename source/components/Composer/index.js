70af6699edf20115a72a0efed31f09f212dc766e
ellyd <meloniakfs27@gmail.com>
2019-09-17 21:07:33 +0300

    classwork 12

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -1,6 +1,6 @@
 //Core
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { func, string } from 'prop-types';
 
 
 //Components 
@@ -9,10 +9,11 @@ import { withProfile } from 'components/HOC/withProfile';
 //Instruments 
 import Styles from './styles.m.css';
 
-@withProfile
-export default class Composer extends Component {
+export class Composer extends Component {
     static propTypes = {
-      _createPost: PropTypes.func.isRequired,
+      _createPost: func.isRequired,
+      avatar: string.isRequired,
+      currentUserFirstName: string.isRequired,
     };
 
 
@@ -74,3 +75,5 @@ export default class Composer extends Component {
       );
     }
 }
+
+export default withProfile(Composer);
