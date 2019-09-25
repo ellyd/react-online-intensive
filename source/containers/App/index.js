92f1d36e9c0209aaca079629322246d2e27c8fdb
ellyd <meloniakfs27@gmail.com>
2019-09-25 18:13:10 +0300

    hw 13

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -6,7 +6,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 //Components
 import Catcher from 'components/Catcher';
 import StatusBar from 'components/StatusBar';
-import News from 'components/News';
 import Login from 'components/Login';
 import Feed from 'components/Feed';
 import Profile from 'components/Profile';
@@ -14,27 +13,54 @@ import { Provider } from 'components/HOC/withProfile';
 
 //Instruments 
 import avatar from 'theme/assets/Lisa';
+import { socket } from 'socket/init';
 
 const options = {
     avatar,
     currentUserFirstName: 'Елизавета',
     currentUserLastName: 'Дмитракова',
+    loggedIn: false,
   }
   
 @hot(module)
 export default class App extends Component {
+    state = {
+        access: false,
+    }
+
+    componentDidMount () {
+        socket.on('login', () => {
+            this.setState({
+                loggedIn: true,
+            });
+        });   
+        
+        socket.on('logout', () => {
+            this.setState({
+                loggedIn: false,
+            });
+        });   
+
+    }
+
+    componentWillUnmount () {
+        socket.removeListener('login');
+        socket.removeListener('logout');
+    }
+
     render() {
+        const { authenticated } = this.props;
+
         return (
             <Catcher>
                 <Provider value = { options }>
                     <StatusBar />
                     <Switch>
-                        <Route component = { News } path = '/news' render={() => (
-                            { News } ? <App /> : <Redirect to = '/login' />
-                        )}/>
                         <Route component = { Feed } path = '/feed' />
                         <Route component = { Profile } path='/profile' />
-                        <Route component = { Login } path = '/login' />
+                        <Route component = { Login } path = '/login' render = {(props) => (
+                            authenticated ? <App/> : <Redirect to = '/login' />
+                        )}/>
                         <Redirect to = '/feed' />
                     </Switch>
                 </Provider>
